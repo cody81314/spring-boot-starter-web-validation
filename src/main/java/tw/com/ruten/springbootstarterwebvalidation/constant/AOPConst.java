0dbef5b8e403a0feab84253cfd16007c1df6b03f
cody81314 <cody81314@staff.ruten.com.tw>
2020-05-14 11:12:37 +0800

    Release 0.0.5

--- src/main/java/tw/com/ruten/springbootstarterwebvalidation/constant/AOPConst.java
+++ src/main/java/tw/com/ruten/springbootstarterwebvalidation/constant/AOPConst.java
@@ -3,7 +3,7 @@ package tw.com.ruten.springbootstarterwebvalidation.constant;
 public class AOPConst {
 
     private static final String POINTCUT_DEFINITION_PATH =
-            "tw.com.ruten.authorizationserver.aop.PointcutDefinition.";
+            "tw.com.ruten.springbootstarterwebvalidation.aop.PointcutDefinition.";
     public static final String POINTCUT_WEBLAYER =
             POINTCUT_DEFINITION_PATH + "webLayer()";
 }
